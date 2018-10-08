599674327b471bc3af1b9c52011672371dac5e14
Ilya27 <iluxan7@gmail.com>
2018-10-08 22:37:52 +0300

    Delete

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$databaseHost = 'localhost';
-$databaseName = 'users2';
-$databaseUsername = 'root';
-$databasePassword = '';
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-?>
